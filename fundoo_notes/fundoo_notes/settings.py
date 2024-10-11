7a65b005c5f41b758609d4429ab1adccba5d540a
unknown <keshav.wanjale@gmail.com>
2024-10-11 14:27:43 +0530

    [Keshav] FEAT: configured mailtrap

--- fundoo_notes/fundoo_notes/settings.py
+++ fundoo_notes/fundoo_notes/settings.py
@@ -138,3 +138,11 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'user.CustomUser'
+
+
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_PORT = os.getenv('EMAIL_PORT')
+EMAIL_USE_TLS = False
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER') 
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
