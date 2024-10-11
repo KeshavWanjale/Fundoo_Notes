5d46f1d86a1d0b63ffa1a1d6767c31af025aa095
unknown <keshav.wanjale@gmail.com>
2024-10-11 14:31:55 +0530

    [Keshav] FEAT: created verify_registered_user function which takes token sent to user on email and verifies it.Updated RegisterUser class to generate token, create verification url and send email to user

--- fundoo_notes/user/views.py
+++ fundoo_notes/user/views.py
@@ -4,29 +4,90 @@ from rest_framework import status
 from django.contrib.auth import authenticate
 
 from .serializer import *
-from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework_simplejwt.tokens import RefreshToken, AccessToken
+from rest_framework.decorators import api_view
+from rest_framework.reverse import reverse
+from django.core.mail import send_mail
+from django.conf import settings
 
-# Create your views here.
 
+@api_view(['GET'])
+def verify_registered_user(request, token):
+    """
+    Description:
+        API view to verify a registered user by decoding the provided JWT token. 
+        If the token is valid, the user's `is_verified` status is updated.
+
+    Parameter:
+        request (Request): The request object containing the user's verification token.
+        token (str): The JWT token sent to verify the user.
+
+    Returns:
+        Response:
+            - On success: A response with a success message and status code 200 if 
+              the user is successfully verified.
+            - On failure: A response with an error message and status code 400 
+              if the token is invalid or expired.
+    """
+    try:
+        access_token = AccessToken(token)
+        user_id = access_token['user_id']
+        user = CustomUser.objects.get(id=user_id)
+
+        if not user.is_verified:
+            user.is_verified = True
+            user.save()
+
+        print(user)
+
+        return Response({"message": "User verification successful", "status": "success"}, status=status.HTTP_200_OK)
+
+    except:
+        return Response({"message": "Invalid or expired token", "status": "error"}, status=status.HTTP_400_BAD_REQUEST)
+    
 
 class RegisterUser(APIView):
     """
     Description:
-        API view to handle user registration. Receives user data via POST 
-        request, validates it using `UserRegistrationSerializer`, and creates 
-        a new user if validation is successful.
+        API view to handle user registration. Registers a user using the data provided 
+        in the POST request, generates a verification JWT token, and sends a verification 
+        email with a link to verify the user's account.
     Parameter:
         request (Request): The request object containing user registration data.
     Returns:
-        Response: A response with a success message and user data if 
-        registration is successful, or an error message with validation errors.
+        Response:
+            - On success: A response with a success message, user data, and a prompt 
+              to verify the user's email. Status code 201.
+            - On failure: A response with an error message detailing validation issues or 
+              other errors. Status code 400.
     """
     def post(self,request):
 
         serializer = UserRegistrationSerializer(data=request.data)
         if serializer.is_valid():
-            serializer.save()
-            return Response({"message": "User registered successfully!", "status": "success", "data": serializer.data}, status=status.HTTP_201_CREATED)
+            user = serializer.save()
+
+            # create token
+            refresh = RefreshToken.for_user(user)
+            token = str(refresh.access_token)
+
+            # Create verifiaction url using reverse
+            verification_url = request.build_absolute_uri(
+                reverse('verify_user',kwargs={'token': token})
+            )
+
+            # send verification mail
+            subject = 'Verify your account'
+            message = f'Hi {user.username},\n\nPlease verify your account using the link below:\n{verification_url}'
+            recipient_list = [user.email]
+              
+            send_mail(subject, message, settings.DEFAULT_FROM_EMAIL, recipient_list)
+
+            return Response({
+                "message": "User registered successfully! Please verify your email.",
+                "status": "success",
+                "data": serializer.data
+            }, status=status.HTTP_201_CREATED)
         
         return Response({"message": "Unexpected error occured", "status": "error", "error": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
     
@@ -70,5 +131,3 @@ class LoginUser(APIView):
             return Response({"message": "Unexpected error occured", "error": "Invalid email or password"}, status=status.HTTP_400_BAD_REQUEST)
         
         return Response({"message": "Unexpected error occured", "status": "error", "error": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
-    
-
